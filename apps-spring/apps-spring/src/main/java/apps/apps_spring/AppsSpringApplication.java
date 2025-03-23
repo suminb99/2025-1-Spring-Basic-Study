c63b334b7c0d000738817b5b2710aa1439eb135e
eunground <jeshin@sookmyung.ac.kr>
2025-03-23 21:40:20 +0900

    Feat: 프로젝트 생성

---
+++ apps-spring/apps-spring/src/main/java/apps/apps_spring/AppsSpringApplication.java
@@ -0,0 +1,13 @@
+package apps.apps_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsSpringApplication.class, args);
+	}
+
+}
