f84aa81a985bd99b2e8a8154804b0bed2b308412
dahyun1060 <dahyun1060@naver.com>
2025-03-28 17:30:13 +0900

    Feat: 2주차 실습 수정

--- team1/엄다현/week2/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ team1/엄다현/week2/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -12,17 +12,14 @@ public class MemoryMemberRepository implements MemberRepository {
         store.put(member.getId(), member);
         return member;
     }
-
     @Override
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
-
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
-
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
