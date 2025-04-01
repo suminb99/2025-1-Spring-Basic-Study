49089f12dab8ce1b61f87cb2684f77040ca616a5
Doyeon Kim <ehdus9809@gmail.com>
2025-04-01 23:12:12 +0900

    Feat: 2주차 과제

---
+++ team2/김도연/week2/hello_spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findByID(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findALL(); /// 지금까지 저장된 모든 회원 리스트 반환
+}
